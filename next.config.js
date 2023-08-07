f46d49906377b5c7995e2c7d2a4eba50a696758d
Juan Jose Vega <juanjosevegaquintero@hotmail.com>
2023-08-06 23:11:25 -0500

    chore: add images next.config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,15 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  images: {
+    // domains: ['cdn.unwire.hk', 'apicms.thestar.com.my'],
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,3 @@
-import Link from 'next/link';
 import Image from 'next/image';
 
 import { PageLayout } from '../components/PageLayout';
@@ -15,6 +14,9 @@ export default function Home({ articles }) {
               <Image
                 alt={`Image for the article ${article.title}`}
                 src={article.urlToImage}
+                width={450}
+                height={300}
+                layout='responsive'
               />
               <h2>{article.title}</h2>
               <p>{article.description}</p>
@@ -27,13 +29,23 @@ export default function Home({ articles }) {
 
 // export async function getServerSideProps() {
 export async function getStaticProps() {
-  const response = await fetch(
-    'https://newsapi.org/v2/everything?q=tesla&from=2023-07-06&sortBy=publishedAt&apiKey=2f15ff4fb9594d90b6236d4711a1c0ea'
-  );
-  const { articles } = await response.json();
-  return {
-    props: {
-      articles,
-    },
-  };
+  try {
+    const response = await fetch(
+      // 'https://newsapi.org/v2/everything?q=tesla&from=2023-07-06&sortBy=publishedAt&apiKey=2f15ff4fb9594d90b6236d4711a1c0ea'
+      'https://newsapi.org/v2/top-headlines?sources=techcrunch&apiKey=2f15ff4fb9594d90b6236d4711a1c0ea'
+    );
+
+    if (!response.ok) {
+      throw new Error('Failed to fetch articles from API');
+    }
+
+    const { articles } = await response.json();
+    return {
+      props: {
+        articles,
+      },
+    };
+  } catch (error) {
+    console.error('Error fetching articles:', error);
+  }
 }
